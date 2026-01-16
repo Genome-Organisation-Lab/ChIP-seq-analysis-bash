f61bc0993e49d103bc634dbcc553a4069caae281
Jessica Taylor <88970363+jessica-a-taylor@users.noreply.github.com>
2026-01-16 17:33:45 +0000

    Add files via upload

---
+++ run_analysis.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+##Coded by Heena Ambreen : h.ambreen@exeter.ac.uk
+##This pipeline is used for peak calling using CHIP-seq data "Paired end"; Adapted from Jess's ChIP-seq-peakcaller @ https://github.com/jessica-a-taylor/ChIP-seq-peakcaller/tree/main
+
+#Usage: bash peakcalling_PE1.sh $1 $2
+
+##Pre-requisites and dependencies
+##sra-toolkit trim-galore bowtie2 samtooos bedtools python3-biopython macs2 
+
+
+##setting parameters and configurations for the pipeline
+
+Test_Rep1=$1
+#Control_Rep1=$2
+##comment out if replicates are present for either test or control samples
+#Test_Rep2=$3
+#Control_Rep2=$4
+
+trimCORES=1 ##change based on core availability
+CORES=1 ##change based on core availability
+
+#Reference genome and basic information information
+#refDir=/home/data/fus-localiser-js-2023/Epigenetics\CUT_Tag\CUT_Tag_PeakCaller_Main #refDir=/path/to/genome/reference
+genome=GCA_900044135.1_GZPH1RResV1_genomic.fna
+index=PH1
+#fastaLength=$(getFastaLength.py !{genomeFasta})
+fastaLength=59900000 ##analysis from the script
+epiMARK=H3K27me3 ##epigenetic mark under study
+genotype=fusarium ##genotype background under study
+
+##path to program executables and directories
+raw_TestDir1=/home/data/fus-localiser-js-2023/Epigenetics/CUT_Tag/CUT_Tag_PeakCaller_Main/$Test_Rep1 ##Rep1
+#raw_ControlDir1=/home/data/fus-localiser-js-2023/Epigenetics/CUT_Tag/CUT_Tag_PeakCaller_Main/$Control_Rep1 ##Rep1
+
+#raw_TestDir2=/mnt/RAID/data/sequencing/Heena/Epigenome/$Test_Rep2 ##Rep2
+#raw_ControlDir2=/mnt/RAID/data/sequencing/Heena/Epigenome/$Control_Rep2 ##Rep2
+
+outDir=/home/data/fus-localiser-js-2023/Epigenetics/CUT_Tag/CUT_Tag_PeakCaller_Main/${genotype}_${epiMARK}/analysis
+mkdir -p $outDir
+
+#refDir=/path/to/genome/reference
+refDir=/home/data/fus-localiser-js-2023/Epigenetics/CUT_Tag/CUT_Tag_PeakCaller_Main
+
+#bowtie2Dir=/usr/bin 
+##add tools path if not at the home directory
+
+
+##0##create epigenetic mark specific library
+mkdir -p $genotype_$epiMARK
+
+##1## Building bowtie2 index for reference genome
+
+bowtie2-build --threads $CORES $refDir/$genome $index
+
+##2## Trim the raw "Test" and "Control" ChIP-seq reads using trim galore: paired end
+##For trim_galore use less number of cores anything around 4 is good otherwise it complains of "using an excessive number of cores has diminishing return". Dont go over 8!.
+
+trim_galore --output_dir $outDir/ --length 30 --quality 20 --stringency 1 -e 0.1 --paired -j $trimCORES $raw_TestDir1/${Test_Rep1}_1.fastq $raw_TestDir1/${Test_Rep1}_2.fastq
+
+#trim_galore --output_dir $outDir/ --length 30 --quality 20 --stringency 1 -e 0.1 --paired -j $trimCORES $raw_ControlDir1/${Control_Rep1}_1.fastq $raw_ControlDir1/${Control_Rep1}_2.fastq
+
+##Un-comment the following commands if if you have "replicates"
+
+#trim_galore --output_dir $outDir/ --length 30 --quality 20 --stringency 1 -e 0.1 --paired -j $CORES $raw_TestDir2/${Test_Rep2}_1.fastq $raw_TestDir2/${Test_Rep2}_2.fastq
+
+#trim_galore --output_dir $outDir/ --length 30 --quality 20 --stringency 1 -e 0.1 --paired -j $CORES $raw_ControlDir2/${Control_Rep2}_1.fastq $raw_ControlDir2/${Control_Rep2}_2.fastq
+
+echo "Trimming finished for all samples"
+
+##3## Align the QC test and control reads against the genome using bowtie2
+
+bowtie2 --threads $CORES --local --very-sensitive-local --no-unal --no-mixed --no-discordant --phred33 -I 10 X 700 -x $refDir/$index -1 $outDir/${Test_Rep1}_1_val_1.fq -2 $outDir/${Test_Rep1}_2_val_2.fq -S $outDir/$Test_Rep1.sam 
+
+samtools view --threads $CORES -bhS $outDir/$Test_Rep1.sam -o $outDir/$Test_Rep1.bam
+
+samtools index --threads $CORES $outDir/${Test_Rep1}.bam
+
+#bowtie2 --threads $CORES --end-to-end --sensitive -x $refDir/$index -1 $outDir/${Test_Rep1}_1_val_1.fq -2 $outDir/${Test_Rep1}_2_val_2.fq -S $outDir/$Test_Rep1.sam 
+
+#bowtie2 --threads $CORES --end-to-end --sensitive -x $refDir/$index -1 $outDir/${Control_Rep1}_1_val_1.fq -2 $outDir/${Control_Rep1}_2_val_2.fq -S $outDir/$Control_Rep1.sam
+
+##bowtie for replicates: paired end
+#bowtie2 --threads $CORES --end-to-end --sensitive -x $refDir/$index -1 $outDir/${Test_Rep2}_1_val_1.fq -2 $outDir/${Test_Rep2}_2_val_2.fq -S $outDir/$Test_Rep2.sam
+
+#bowtie2 --threads $CORES --end-to-end --sensitive -x $refDir/$index -1 $outDir/${Control_Rep2}_1_val_1.fq -2 $outDir/${Control_Rep2}_2_val_2.fq -S $outDir/$Control_Rep2.sam
+
+#echo "Mapping finished for all samples"
+
+##4## Use samtools to remove PCR duplicates: rmdup is deprecated so using markdup
+
+#samtools rmdup  --output-fmt BAM $outDir/$Test_Rep1.sam $outDir/$Test_Rep1_deduplicated.bam
+#samtools rmdup  --output-fmt BAM $outDir/$Control_Rep1.sam $outDir/$Control_Rep1_deduplicated.bam
+#samtools rmdup  --output-fmt BAM $outDir/$Test_Rep2.sam $outDir/$Test_Rep2_deduplicated.bam ##samtools rmdup for replicates: paired end
+#samtools rmdup  --output-fmt BAM $outDir/$Control_Rep2.sam $outDir/$Control_Rep2_deduplicated.bam ##samtools rmdup for replicates: paired end
+
+#samtools sort --threads $CORES -n $outDir/${Test_Rep1}.sam -o $outDir/${Test_Rep1}_sorted.sam 
+#samtools fixmate --threads $CORES -m $outDir/${Test_Rep1}_sorted.sam $outDir/${Test_Rep1}_sorted.scored.sam  # m - add mate tag
+#samtools sort --threads $CORES $outDir/${Test_Rep1}_sorted.scored.sam -o $outDir/${Test_Rep1}_sorted.scored.sorted.sam
+#samtools markdup --threads $CORES -O BAM -r $outDir/${Test_Rep1}_sorted.scored.sorted.sam $outDir/${Test_Rep1}_deduplicated.bam #r - remove duplicate reads
+
+#samtools sort --threads $CORES -n $outDir/${Control_Rep1}.sam -o $outDir/${Control_Rep1}_sorted.sam 
+#samtools fixmate --threads $CORES -m $outDir/${Control_Rep1}_sorted.sam $outDir/${Control_Rep1}_sorted.scored.sam 
+#samtools sort --threads $CORES $outDir/${Control_Rep1}_sorted.scored.sam -o $outDir/${Control_Rep1}_sorted.scored.sorted.sam
+#samtools markdup --threads $CORES -O BAM -r $outDir/${Control_Rep1}_sorted.scored.sorted.sam $outDir/${Control_Rep1}_deduplicated.bam
+
+#samtools sort --threads $CORES -n $outDir/${Test_Rep2}.sam -o $outDir/${Test_Rep2}_sorted.sam 
+#samtools fixmate --threads $CORES -m $outDir/${Test_Rep2}_sorted.sam $outDir/${Test_Rep2}_sorted.scored.sam 
+#samtools sort --threads $CORES $outDir/${Test_Rep2}_sorted.scored.sam -o $outDir/${Test_Rep2}_sorted.scored.sorted.sam
+#samtools markdup --threads $CORES -O BAM -r $outDir/${Test_Rep2}_sorted.scored.sorted.sam $outDir/${Test_Rep2}_deduplicated.bam
+
+#samtools sort --threads $CORES -n $outDir/${Control_Rep2}.sam -o $outDir/${Control_Rep2}_sorted.sam 
+#samtools fixmate --threads $CORES -m $outDir/${Control_Rep2}_sorted.sam $outDir/${Control_Rep2}_sorted.scored.sam 
+#samtools sort --threads $CORES $outDir/${Control_Rep2}_sorted.scored.sam -o $outDir/${Control_Rep2}_sorted.scored.sorted.sam
+#samtools markdup --threads $CORES -O BAM -r $outDir/${Control_Rep2}_sorted.scored.sorted.sam $outDir/${Control_Rep2}_deduplicated.bam
+
+
+#samtools index --threads $CORES $outDir/${Test_Rep1}.bam
+#samtools index --threads $CORES $outDir/${Test_Rep2}_deduplicated.bam
+#samtools index --threads $CORES $outDir/${Control_Rep1}_deduplicated.bam
+#samtools index --threads $CORES $outDir/${Control_Rep2}_deduplicated.bam
+
+echo "PCR duplicates removed"
+
+##5## Call both broad and narrow peaks using the MACS2 peakcaller
+
+#source activate nextflow
+
+#source activate MACS2
+
+macs2 callpeak -g $fastaLength –p 1e-5 -t $outDir/${Test_Rep1}.bam --outdir $outDir --name ${Test_Rep1}_narrow -f BAMPE –keep-dup all
+
+#macs2 callpeak—t input_file –p 1e-5 –f BEDPE/BED(Paired End vs. Single End sequencing data) –keep-dup all –n out_name.
+
+#macs2 callpeak -g $fastaLength -q 0.05 -t $outDir/${Test_Rep1}_deduplicated.bam --outdir $outDir --name ${Test_Rep1}_narrow -f BAMPE --nomodel --extsize 200 
+
+#macs2 callpeak -c $outDir/${Control_Rep1}_deduplicated.bam --broad -g $fastaLength -q 0.00001 -t $outDir/${Test_Rep1}_deduplicated.bam --outdir $outDir --name ${Test_Rep1}_broad -f BAMPE  --broad-cutoff 0.00001
+
+#macs2 callpeak -c $outDir/${Control_Rep2}_deduplicated.bam -g $fastaLength -q 0.00001 -t $outDir/${Test_Rep2}_deduplicated.bam --outdir $outDir --name ${Test_Rep2}_narrow -f BAMPE
+
+#macs2 callpeak -c $outDir/${Control_Rep2}_deduplicated.bam --broad -g $fastaLength -q 0.00001 -t $outDir/${Test_Rep2}_deduplicated.bam --outdir $outDir --name ${Test_Rep2}_broad -f BAMPE --broad-cutoff 0.00001
+
+conda deactivate 
+
+echo "Peak calling finished"
+
+##Merge replicates 
+
+cat $outDir/*_peaks.narrowPeak > $outDir/unmergedNarrowPeaks.bed
+sort -k1,1 -k2,2n $outDir/unmergedNarrowPeaks.bed > $outDir/unmergedNarrowPeaks.sorted.bed
+bedtools merge -i $outDir/unmergedNarrowPeaks.sorted.bed > $outDir/${genotype}_${epiMARK}_mergedNarrowPeaks.bed
+
+
+cat $outDir/*_peaks.broadPeak > $outDir/unmergedBroadPeaks.bed
+sort -k1,1 -k2,2n $outDir/unmergedBroadPeaks.bed > $outDir/unmergedBroadPeaks.sorted.bed
+bedtools merge -i $outDir/unmergedBroadPeaks.sorted.bed > $outDir/${genotype}_${epiMARK}_mergedBroadPeaks.bed
+    
+#rm $outDir/*.sam
+rm $outDir/*.fq
+rm $outDir/*_trimming_report.txt
+
+    
+echo "peak_calling pipeline finished for ${genotype}_${epiMARK}"
